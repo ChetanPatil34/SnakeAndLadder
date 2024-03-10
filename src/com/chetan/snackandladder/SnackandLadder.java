92247b4acee6e638419ea0548de6e478f6a3a20e
ChetanPatil34 <cp9210639@gmail.com>
2024-03-10 23:51:17 +0530

    [MODIFIED]:Chetan - UC-3 Check Dies Option

--- src/com/chetan/snackandladder/SnackandLadder.java
+++ src/com/chetan/snackandladder/SnackandLadder.java
@@ -6,6 +6,10 @@ public class SnackandLadder {
 
 	private int playerPosition = 0;
 	private int rolldie = 0;
+	private int dieOption = 0;
+	private static final int NO_PLAY = 0;
+	private static final int LADDER = 1;
+	private static final int SNAKE = 2;
 	
 	public void showPosition()
 	{
@@ -17,7 +21,33 @@ public class SnackandLadder {
 		rolldie = random.nextInt(6)+1;
 		System.out.println("Player Die Roll = " + rolldie);
 		
+	   dieOption = random.nextInt(3);
+	   System.out.println("Die Position = " + dieOption);
+	   
+	   switch(dieOption)
+	   {
+	   case NO_PLAY:
+		   
+		   playerPosition = playerPosition ;
+		   break ;
+		   
+	   case LADDER:
+		   
+		   playerPosition = playerPosition + rolldie;
+		   break;
+		   
+	   case SNAKE:
+		   
+		   playerPosition = playerPosition - rolldie;
+		   if(playerPosition < 0)
+		   {
+		      playerPosition = 0;
+	       }
+		   break;
+	   }
 	}
+	
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Well Come to SnackLadder Project");
@@ -25,6 +55,7 @@ public class SnackandLadder {
 		SnackandLadder snackandladder = new SnackandLadder();
 		snackandladder.showPosition();
 		snackandladder.playgame();
+		snackandladder.showPosition();
 		
 		
 	}
