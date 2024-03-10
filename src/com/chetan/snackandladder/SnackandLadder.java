6a3470b70a842b64d8632b8ac14f2223c1445c03
ChetanPatil34 <cp9210639@gmail.com>
2024-03-10 22:30:19 +0530

    [MODIFIED]:Chetan - UC-1 Player Start Position

--- src/com/chetan/snackandladder/SnackandLadder.java
+++ src/com/chetan/snackandladder/SnackandLadder.java
@@ -2,7 +2,17 @@ package com.chetan.snackandladder;
 
 public class SnackandLadder {
 
+	private int playerPosition = 0;
+	
+	public void showPosition()
+	{
+		System.out.println("Player Position = " + playerPosition );
+	}
+	
 	public static void main(String[] args) {
+		
+		SnackandLadder snackandladder = new SnackandLadder();
+		snackandladder.showPosition();
 		
 		System.out.println("Well Come to SnackLadder Project");
 	}
