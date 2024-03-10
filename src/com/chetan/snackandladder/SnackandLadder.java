686c2e8296b72ce91b5c39e5e2393d681e132e33
ChetanPatil34 <cp9210639@gmail.com>
2024-03-10 22:48:33 +0530

    [MODIFIED]:Chetan - UC-2 Player Roll Die

--- src/com/chetan/snackandladder/SnackandLadder.java
+++ src/com/chetan/snackandladder/SnackandLadder.java
@@ -1,20 +1,32 @@
 package com.chetan.snackandladder;
 
+import java.util.Random;
+
 public class SnackandLadder {
 
 	private int playerPosition = 0;
+	private int rolldie = 0;
 	
 	public void showPosition()
 	{
 		System.out.println("Player Position = " + playerPosition );
 	}
+	public void playgame()
+	{
+		Random random = new Random();
+		rolldie = random.nextInt(6)+1;
+		System.out.println("Player Die Roll = " + rolldie);
+		
+	}
 	
 	public static void main(String[] args) {
+		System.out.println("Well Come to SnackLadder Project");
 		
 		SnackandLadder snackandladder = new SnackandLadder();
 		snackandladder.showPosition();
+		snackandladder.playgame();
 		
-		System.out.println("Well Come to SnackLadder Project");
+		
 	}
 
 }
